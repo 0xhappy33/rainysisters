de1232fd545bd6abb50da544277849cb34fb8ef1
hpcity <happy.city.community@gmail.com>
2017-11-05 10:40:53 +0700

    add lib main.js

--- App/Views/layout/layout.php
+++ App/Views/layout/layout.php
@@ -23,4 +23,10 @@
     <link rel="stylesheet" href="../css/top.css">
     <link rel="stylesheet" href="../css/login.css">
 
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
+    <!-- read -->
+    <link rel="stylesheet" href="../css/read.css">
+    <link rel="stylesheet" href="../css/font-awesome.min.css">
+	<link rel="stylesheet" href="../css/animate.css">
+    <!-- end read -->
 </head>
