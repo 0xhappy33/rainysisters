22909477d14986a5035602e06a1a355168ec4f51
hpcity <happy.city.community@gmail.com>
2017-11-05 10:27:53 +0700

    add css header and footer

--- public/template/layout.php
+++ public/template/layout.php
@@ -5,4 +5,6 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
+    <link rel="stylesheet" href="/public/css/header.css">
+    <link rel="stylesheet" href="/public/css/footer.css">
 </head>
