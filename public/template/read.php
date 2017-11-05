f72ca0f394224d5fd16038816bf588316fd81786
hpcity <happy.city.community@gmail.com>
2017-11-05 09:58:02 +0700

    Initial commit

---
+++ public/template/read.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html lang="en">
+<?php include 'layout.php'; ?>
+<body>
+<div id="wrapper">
+    <?php include 'header.php'; ?>
+    <main>
+
+    </main>
+    <?php include 'footer.php';?>
+    <?php include 'script.php'; ?>
+</div>
+</body>
+</html>
---
+++ public/template/script.php
@@ -0,0 +1,1 @@
+<script src="abc.js"></script>
