08dfe4629ec174a07e4b567d9ebb407e7f186c23
NuongNguyen1996 <nuong14pt@gmail.com>
2017-11-17 00:46:04 +0700

    finish task read on read branch

--- public/template/read.php
+++ public/template/read.php
@@ -2,64 +2,65 @@
 <html lang="en">
 <?php include 'layout.php'; ?>
 <body>
-<div id="wrapper">
-    <?php include 'header.php'; ?>
-    <main>
-		<div id="top"></div>
+
+	<div id="top">
+		<?php include 'header.php'; ?>
+	</div>
 	<div class="container-fluid " id = "wrapper">
 	<div id = "content-top">
 
+
 		<div class="row">
-			<div class="col-md-2">
+			<div class="col-md-2 col-xs-1 col-sm-2">
 			<div class = "list-social" id = "list-social">
 				<ul calss = "social">
 					<li>
 						<div class="like wow rotateIn" data-wow-offset = "0" >
 							<a>
-								<span class=" glyphicon glyphicon-thumbs-up" title = "Like">
-									
+								<span class="fa fa-thumbs-o-up" title = "Like">
+
 								</span></a>
-								
+
 								</div>
 					</li>
 					<li>
 						<div class="heart wow rotateIn" >
 							<a>
 								<span class=" fa fa-heart-o" title = "Love" >
-									
+
 								</span>
 							</a>
-								
+
 						</div>
 					</li>
 					<li>
 						<div class="like wow rotateIn">
 							<a href="">
-								<span class = " glyphicon glyphicon-comment" title = "Comment">
-									
+								<span class = "fa fa-comment" title = "Comment">
+
 								</span>
 							</a>
 
 						</div>
 					</li>
 					<li>
-						<div class="like wow rotateIn">	
+						<div class="like wow rotateIn">
 							<a href="">
 								<span class = "fa fa-share-alt-square" title = "Share"></span>
-							</a>	
+							</a>
 						</div>
 					</li>
 				</ul>
 				</div>
 			</div>
-			<div class="col-md-8">
-				
+			<div class="col-md-8 col-xs-10 col-sm-8">
+
 
 				<div>
 					<div class = "row">
 						<div class="col-md-1">
-							<img src="https://cdn-images-1.medium.com/fit/c/60/60/1*rYalTSXIZUoXfxPgB7VqiA.jpeg" class="avatar-image avatar-image--small" alt="Go to the profile of user">
-							
+							<img src="../images/anh1.png" class="avatar-image avatar-image--small" alt="Go to the profile of user">
+
 						</div>
 						<div class="col-md-11">
 							<a class = "name-user" href="https://medium.com/@farnamstreet?source=post_header_lockup" dir="auto">
@@ -74,10 +75,10 @@
 								<span class = "detail-user">3 min read</span>
 							</div>
 						</div>
-						
+
 					</div>
 					<div class = "pic">
-						<img src="../images/anh1.png" alt="">
+						<img src="../images/anh1.png" alt=""  max-width="100%">
 						<figcaption class="detail-user">Photo via Shane&nbsp;Parrish</figcaption>
 					</div>
 					<div class = "main-content">
@@ -87,7 +88,7 @@
 
 						<p>Ray Dalio wasn’t always a billionaire. In fact, he punched his boss in the face, nearly went broke multiple times, and ran a fledgling business from a two-bedroom apartment in New York.</p>
 						<p>What he was doing wasn’t working. But unlike most people, when the chips were down, Ray didn’t blame the world. Rather, he confronted reality and vowed to change.</p>
-						<p>Below are three strategies, pulled from an interview he gave on 
+						<p>Below are three strategies, pulled from an interview he gave on
 							<a href="https://www.farnamstreetblog.com/2017/10/ray-dalio/" data-href="https://www.farnamstreetblog.com/2017/10/ray-dalio/" target="_blank">Farnam Street’s podcast The Knowledge Project</a>
 							, that will help you see reality, find clarity, and make better decisions.</p>
 
@@ -101,7 +102,7 @@
 
 							<p>Ray Dalio wasn’t always a billionaire. In fact, he punched his boss in the face, nearly went broke multiple times, and ran a fledgling business from a two-bedroom apartment in New York.</p>
 							<p>What he was doing wasn’t working. But unlike most people, when the chips were down, Ray didn’t blame the world. Rather, he confronted reality and vowed to change.</p>
-							<p>Below are three strategies, pulled from an interview he gave on 
+							<p>Below are three strategies, pulled from an interview he gave on
 							<a href="https://www.farnamstreetblog.com/2017/10/ray-dalio/" data-href="https://www.farnamstreetblog.com/2017/10/ray-dalio/" target="_blank">Farnam Street’s podcast The Knowledge Project</a>
 							, that will help you see reality, find clarity, and make better decisions.</p>
 
@@ -116,7 +117,7 @@
 
 							<p>Ray Dalio wasn’t always a billionaire. In fact, he punched his boss in the face, nearly went broke multiple times, and ran a fledgling business from a two-bedroom apartment in New York.</p>
 							<p>What he was doing wasn’t working. But unlike most people, when the chips were down, Ray didn’t blame the world. Rather, he confronted reality and vowed to change.</p>
-							<p>Below are three strategies, pulled from an interview he gave on 
+							<p>Below are three strategies, pulled from an interview he gave on
 							<a href="https://www.farnamstreetblog.com/2017/10/ray-dalio/" data-href="https://www.farnamstreetblog.com/2017/10/ray-dalio/" target="_blank">Farnam Street’s podcast The Knowledge Project</a>
 							, that will help you see reality, find clarity, and make better decisions.</p>
 
@@ -131,7 +132,7 @@
 
 							<p>Ray Dalio wasn’t always a billionaire. In fact, he punched his boss in the face, nearly went broke multiple times, and ran a fledgling business from a two-bedroom apartment in New York.</p>
 							<p>What he was doing wasn’t working.<span class = "note-hight-light">But unlike most people, when the chips were down, </span> Ray didn’t blame the world. Rather, he confronted reality and vowed to change.</p>
-							<p>Below are three strategies, pulled from an interview he gave on 
+							<p>Below are three strategies, pulled from an interview he gave on
 							<a href="https://www.farnamstreetblog.com/2017/10/ray-dalio/" data-href="https://www.farnamstreetblog.com/2017/10/ray-dalio/" target="_blank">Farnam Street’s podcast The Knowledge Project</a>
 							, that will help you see reality, find clarity, and make better decisions.</p>
 
@@ -154,7 +155,7 @@
 						</div>
 
 						<div>
-							
+
 								<ul class = "tag wow bounceInLeft">
 									<li class = "active">
 										<a href="https://medium.com/tag/startup?source=post">
@@ -179,62 +180,62 @@
 										</a>
 									</li>
 								</ul>
-							
+
 						</div>
 
 
-						<div>
+						<!-- <div>
 							<hr class="section-divider">
 
-						</div>
+						</div> -->
 			<!-- chia se, like, tym,... -->
-						<div calss = "social">
+					<!-- 	<div calss = "social">
 							<div class="row">
-								<div class="col-md-2">
+								<div class="col-md-2 col-xs-2 col-sm-2">
 
 								<div class="like wow rotateIn" data-wow-offset = "0" >
-								<a><span class=" glyphicon glyphicon-thumbs-up" title = "Like"></span></a>
-								
+								<a><span class="fa fa-thumbs-o-up" title = "Like"></span></a>
+
 								</div>
 								<span class="qual">9</span>
 								<div class="heart wow rotateIn" >
 								<a><span class=" fa fa-heart-o" title = "Love" ></span></a>
-								
+
 								</div>
-							
+
 							</div>
-							<div class ="col-md-offset-10">
+							<div class ="col-md-offset-10 col-xs-offset-10 col-sm-offset-10">
 								<div class="like wow rotateIn"><a href="">
-									<span class = " glyphicon glyphicon-comment" title = "Comment"></span>
+									<span class = "fa fa-comment" title = "Comment"></span>
 								</a>
 
 								</div>
 								<span class="qual">100</span>
 								<div class="like wow rotateIn">	<a href="">
 									<span class = "fa fa-share-alt-square" title = "Share"></span>
-								</a>	
+								</a>
 								</div>
-								
+
 							</div>
 
-						
-							
-							</div>
-						
-							
 
-						</div>
+
+							</div>
+
+
+
+						</div> -->
 <!-- end social -->
 
 						<div>
 							<hr class="section-divider">
 
 						</div>
-						
+
 					</div>
 				</div>
 				</div>
-				<div class="col-md-2"></div>
+				<div class="col-md-2 col-xs-1 col-sm-2"></div>
 	</div>
 	</div>
 
@@ -251,7 +252,7 @@
 					<div class = "rel-cat">
 					<div class="row">
 						<!-- <div class="col-md-1"></div> -->
-						<div class="col-md-4 item-review">
+						<div class="col-md-4 col-xs-4 col-sm-4 item-review">
 							<div class="bac" style = " background-image: url('../images/anh1.png'); ">
 							<!-- ??? -->
 
@@ -263,15 +264,15 @@
 								</div>
 								<div>
 									<div class = "row">
-										<div class="col-md-2">
+										<div class="ccol-md-2 col-xs-2 col-sm-2">
 											<img src="https://cdn-images-1.medium.com/fit/c/60/60/1*rYalTSXIZUoXfxPgB7VqiA.jpeg" class="avatar-image avatar-image--small" alt="Go to the profile of user">
-											
+
 										</div>
-										<div class="col-md-6">
+										<div class="col-md-6 col-xs-6 col-sm-6">
 											<a class = "name-user" href="https://medium.com/@farnamstreet?source=post_header_lockup" dir="auto">
 											Shane Parrish
 											</a>
-											
+
 											<div class = "detail-user">
 												<time class = "detail-user" datetime="2017-10-14T14:08:53.436Z">Oct 14</time>
 												<span class = "detail-user">.</span>
@@ -279,19 +280,19 @@
 											</div>
 										</div>
 
-										<div class = "col-md-offset-8">
+										<div class = "col-md-offset-8 col-xs-offset-8 col-sm-offset-8">
 											<div class="like wow rotateIn" >
-												<a><span class=" glyphicon glyphicon-thumbs-up" ></span></a>
+												<a><span class="fa fa-thumbs-o-up" ></span></a>
 											</div>
 										</div>
-										
+
 								</div>
 								</div>
 							</div>
 						</div>
-						
-						<div class="col-md-4 item-review">
-							<div class="bac" style = " background-image: url(../images/anh1.png);">
+
+					<div class="col-md-4 col-xs-4 col-sm-4 item-review">
+							<div class="bac" style = " background-image: url('../images/anh1.png'); ">
 							<!-- ??? -->
 
 							</div>
@@ -302,15 +303,15 @@
 								</div>
 								<div>
 									<div class = "row">
-										<div class="col-md-2">
+										<div class="ccol-md-2 col-xs-2 col-sm-2">
 											<img src="https://cdn-images-1.medium.com/fit/c/60/60/1*rYalTSXIZUoXfxPgB7VqiA.jpeg" class="avatar-image avatar-image--small" alt="Go to the profile of user">
-											
+
 										</div>
-										<div class="col-md-6">
+										<div class="col-md-6 col-xs-6 col-sm-6">
 											<a class = "name-user" href="https://medium.com/@farnamstreet?source=post_header_lockup" dir="auto">
 											Shane Parrish
 											</a>
-											
+
 											<div class = "detail-user">
 												<time class = "detail-user" datetime="2017-10-14T14:08:53.436Z">Oct 14</time>
 												<span class = "detail-user">.</span>
@@ -318,18 +319,17 @@
 											</div>
 										</div>
 
-										<div class = "col-md-offset-8">
+										<div class = "col-md-offset-8 col-xs-offset-8 col-sm-offset-8">
 											<div class="like wow rotateIn" >
-												<a><span class=" glyphicon glyphicon-thumbs-up" ></span></a>
+												<a><span class="fa fa-thumbs-o-up" ></span></a>
 											</div>
 										</div>
-										
+
 								</div>
 								</div>
 							</div>
-						</div>
-					<div class="col-md-4 item-review">
-							<div class="bac" style = " background-image: url(../images/anh1.png);">
+						</div><div class="col-md-4 col-xs-4 col-sm-4 item-review">
+							<div class="bac" style = " background-image: url('../images/anh1.png'); ">
 							<!-- ??? -->
 
 							</div>
@@ -340,15 +340,15 @@
 								</div>
 								<div>
 									<div class = "row">
-										<div class="col-md-2">
+										<div class="col-md-2 col-xs-2 col-sm-2">
 											<img src="https://cdn-images-1.medium.com/fit/c/60/60/1*rYalTSXIZUoXfxPgB7VqiA.jpeg" class="avatar-image avatar-image--small" alt="Go to the profile of user">
-											
+
 										</div>
-										<div class="col-md-6">
+										<div class="col-md-6 col-xs-6 col-sm-6">
 											<a class = "name-user" href="https://medium.com/@farnamstreet?source=post_header_lockup" dir="auto">
 											Shane Parrish
 											</a>
-											
+
 											<div class = "detail-user">
 												<time class = "detail-user" datetime="2017-10-14T14:08:53.436Z">Oct 14</time>
 												<span class = "detail-user">.</span>
@@ -356,42 +356,43 @@
 											</div>
 										</div>
 
-										<div class = "col-md-offset-8">
+										<div class = "col-md-offset-8 col-xs-offset-8 col-sm-offset-8">
 											<div class="like wow rotateIn" >
-												<a><span class=" glyphicon glyphicon-thumbs-up" ></span></a>
+												<a><span class="fa fa-thumbs-o-up" ></span></a>
 											</div>
 										</div>
-										
+
 								</div>
 								</div>
 							</div>
 						</div>
-						
+
 					</div>
 					</div>
 					</center>
 <!-- end relation cat -->
-				
+
 
 				</div>
 				<div>
 					<hr class="section-divider">
 				</div>
+				<div class = "container">
 
 				<div class="row">
-					<!-- <div class="col-md-2"></div> -->
-					<div class="col-md-offset-2">
+					<!-- <div class="col-md-3"></div> -->
+					<div class="col-md-offset-3 col-xs-12 col-sm-offset-3">
 						<!-- comment -->
 					<div class="cmt">
 						<h3 >Comment</h3>
 					</div>
 					<div class = "comment">
-						<a href="#"><img src="../images/lanhuong.jpg" alt="not load picture" title = "click here to see more infomation"></a>
+						<a href="#"><img src="../images/anh1.png" alt="not load picture" title = "click here to see more infomation"></a>
 						<form  action="" class = "wow flipInX">
 							<textarea  cols = "65" rows="3" placeholder="Write a comment..."></textarea>
 						</form>
 					</div>
-						
+
 <!-- end comment -->
 
 						<div>
@@ -400,7 +401,7 @@
 						</div>
 <!-- cac binh luan truoc -->
 					<div class = "text-cmt">
-						<a href="#"><img src="../images/lanhuong.jpg" alt="not load picture" title = "click here to see more infomation"><span class = "user-name">Adam BowCut</span><p class = "time-cmt">Oct 15</p></a>
+						<a href="#"><img src="../images/anh1.png" alt="not load picture" title = "click here to see more infomation"><span class = "user-name">Adam BowCut</span><p class = "time-cmt">Oct 15</p></a>
 						<p class = "container content-cmt">
 							valuable wisdom. Thank you!!!
 						</p>
@@ -408,30 +409,30 @@
 										<div calss = "social">
 							<div class="row">
 								<div class="col-md-2">
-
-								<div class="like" >
-								<a><span class=" glyphicon glyphicon-thumbs-up" ></span></a>
-								
-								</div>
-								<span class="qual">9</span>
 								<div class="heart" >
 								<a><span class=" fa fa-heart-o" ></span></a>
-								
+
 								</div>
-							
+								<div class="like" >
+								<a><span class="fa fa-thumbs-o-up" ></span></a>
+
+								</div>
+								<span class="qual">9</span>
+
+
 							</div>
-							
+
 							</div>
-						
-							
+
+
 
 						</div>
 <!-- end social -->
-						
+
 					</div>
 
 					<div class = "text-cmt">
-						<a href="#"><img src="../images/lanhuong.jpg" alt="not load picture" title = "click here to see more infomation"><span class = "user-name">Adam BowCut</span><p class = "time-cmt">Oct 15</p></a>
+						<a href="#"><img src="../images/anh1.png" alt="not load picture" title = "click here to see more infomation"><span class = "user-name">Adam BowCut</span><p class = "time-cmt">Oct 15</p></a>
 						<p class = "container content-cmt">
 							valuable wisdom. Thank you!!!
 						</p>
@@ -439,49 +440,50 @@
 										<div calss = "social">
 							<div class="row">
 								<div class="col-md-2">
-		
-								<div class="like" >
-								<a><span class=" glyphicon glyphicon-thumbs-up" ></span></a>
-								
-								</div>
-								<span class="qual">9</span>
 								<div class="heart" >
 								<a><span class=" fa fa-heart-o" ></span></a>
-								
+
 								</div>
-							
+								<div class="like" >
+								<a><span class="fa fa-thumbs-o-up" ></span></a>
+
+								</div>
+								<span class="qual">9</span>
+
+
 							</div>
-							
+
 							</div>
-						
-							
+
+
 
 						</div>
 <!-- end social -->
-						
+
 					</div>
 					<!-- <div class="col-md-2"></div> -->
 				</div>
 
 
-						
+
 					</div>
 <!-- end -->
 					<!-- end rieng -->
 
 			</div>
-				
+
 	<!-- <div id="on_top">
 		<a href="#top"><img src="images/up.png" alt="up to header"></a>
 	</div> -->
+<div class="top">
+	<a href="#" style = "color: #ccc; font-size: 24px;"><i class="sc fa fa-angle-up" id = "cuon" aria-hidden="true"></i></a>
+</div>
+	
 
-	<a href="#"><i class="sc fa fa-hand-pointer-o" id = "cuon" aria-hidden="true"></i></a>
-			
 		</div>
 	</div>
-    </main>
-    <?php include 'footer.php';?>
-    <?php include 'script.php'; ?>
-</div>
+	<?php include 'footer.php'; ?>
+	<?php include 'script.php'; ?>
 </body>
+
 </html>
