3f4a272932213ff497d869314913691f2ca1b4c4
hpcity <happy.city.community@gmail.com>
2017-11-23 06:16:28 +0700

    abc

---
+++ public/template/header.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+</head>
+
+<body>	
+<div class="container-fluid">
+<header>
+  <nav class="navbar menu">
+    <div class="navbar-header">
+    	<button class="navbar-toggle" type="button" data-toggle="collapse" data-target=".js-navbar-collapse">
+			<span class="sr-only">Toggle navigation</span>
+			<span class="icon-bar"></span>
+			<span class="icon-bar"></span>
+			<span class="icon-bar"></span>
+		</button>
+	</div>
+	<div class="collapse navbar-collapse js-navbar-collapse">
+		 <ul class="nav navbar-nav navbar-right ">
+      <li><a href="#">Home</a></li>
+			<li class="dropdown mega-dropdown">
+				<a href="#" class="dropdown-toggle" data-toggle="dropdown">Categories <span class="caret"></span></a>				
+				<ul class="dropdown-menu mega-dropdown-menu">
+					<li class="col-sm-2">
+						<ul>
+							<li class="dropdown-header">Technology</li>                            
+						</ul>
+					</li>
+					<li class="col-sm-2">
+						<ul>
+							<li class="dropdown-header">Creativity</li>
+						</ul>
+					</li>
+					<li class="col-sm-2">
+						<ul>
+							<li class="dropdown-header">Self</li>						
+						</ul>
+					</li>
+					<li class="col-sm-2">
+						<ul>
+							<li class="dropdown-header">Culture</li>                       
+						</ul>
+					</li>
+          <li class="col-sm-2">
+            <ul>
+              <li class="dropdown-header">Entrepreneuship</li>                       
+            </ul>
+          </li>
+          <li class="col-sm-2">
+            <ul>
+              <li class="dropdown-header">Politics</li>                       
+            </ul>
+          </li>
+				</ul>				
+			</li>
+            
+            <li><a href="#">Review</a></li>
+            <li><a href="#">Contact</a></li>
+            <li><a href="#">Login</a></li>
+            <form class="navbar-form" role="search">
+					<div class="input-group">
+						<input type="text" class="form-control pull-right" style="width: 300px; margin-right: 35px, border: 1px solid black; background-color: #e5e5e5;" placeholder="Search">
+						<span class="input-group-btn">
+							<button type="reset" class="btn btn-default">
+								<span class="glyphicon glyphicon-remove">
+									<span class="sr-only">Close</span>
+								</span>
+							</button>
+							<button type="submit" class="btn btn-default">
+								<span class="glyphicon glyphicon-search">
+									<span class="sr-only">Search</span>
+								</span>
+							</button>
+						</span>
+					</div>
+				</form>
+		</ul>
+     
+	</div><!-- /.nav-collapse -->
+  </nav>
+</div>
+<!-- <script src="../js/header.js"></script> -->
+</body>
+</html>
+
