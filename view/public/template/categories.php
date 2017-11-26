2e3cd37be9a5895d6fb16d5699c57e5e3301d860
NuongNguyen1996 <nuong14pt@gmail.com>
2017-11-26 23:16:14 +0700

    finish design categories html

---
+++ view/public/template/categories.php
@@ -0,0 +1,164 @@
+<!doctype html>
+<html lang="en">
+<?php include 'layout.php'; ?>
+<body>
+	
+	<div id="top">
+		<?php include 'header.php'; ?>
+	</div>
+	<div class="container" id = "wrapper-cate">
+		<div id = "content-top">
+			<header class="u">
+				<h1 class="topic">Explore topics</h1>
+			</header>
+		</div>
+
+		<header class="flex">
+			<span class="title wow SlideInLeft">
+				Arts & Entertainment
+			</span>
+		</header>
+
+		<div id="content-main">
+			<section class="list-cat container">
+				<div class="row">
+					<div class="col-sm-4 item-list-cat">
+						<div class="item-top">
+							<a href="#">Art</a>
+							<button class="detail">
+								<i class="fa fa-plus-circle">
+								</i>
+							</button>
+						</div>
+						<a href="https://medium.com/topic/art" class="">
+							<div class="item-pic" style = " background-image: url('../images/anh1.png'); background-position: 50% 50% !important; " >
+							</div>
+						 </a>	
+					</div>
+
+					<div class="col-sm-4 item-list-cat">
+						<div class="item-top">
+							<a href="#">Art</a>
+							<button class="detail">
+								<i class="fa fa-plus-circle">
+								</i>
+							</button>
+						</div>
+						<a href="https://medium.com/topic/art" class="">
+							<div class="item-pic" style = " background-image: url('../images/anh1.png'); background-position: 50% 50% !important; " >
+							</div>
+						 </a>
+					</div>
+
+					<div class="col-sm-4 item-list-cat">
+						<div class="item-top">
+							<a href="#">Art</a>
+							<button class="detail">
+								<i class="fa fa-plus-circle">
+								</i>
+							</button>
+						</div>
+						<a href="https://medium.com/topic/art" class="">
+							<div class="item-pic" style = " background-image: url('../images/anh1.png'); background-position: 50% 50% !important; " >
+							</div>
+						 </a>
+					</div>
+				</div>
+			</section>			
+		
+
+		</div>
+
+		<header class="flex">
+			<span class="title wow SlideInLeft">
+				Arts & Entertainment
+			</span>
+		</header>
+
+		<div id="content-main">
+			<section class="list-cat">
+				<div class="row">
+					<div class="col-md-4 item-list-cat">
+						<div class="item-top">
+							<a href="#">Art</a>
+							<button class="detail">
+								<i class="fa fa-plus-circle">
+								</i>
+							</button>
+						</div>
+						<a href="https://medium.com/topic/art" class="">
+							<div class="item-pic" style = " background-image: url('../images/anh1.png'); background-position: 50% 50% !important; " >
+							</div>
+						 </a>
+					</div>
+
+					<div class="col-md-4 item-list-cat">
+						<div class="item-top">
+							<a href="#">Art</a>
+							<button class="detail">
+								<i class="fa fa-plus-circle">
+								</i>
+							</button>
+						</div>
+						<a href="https://medium.com/topic/art" class="">
+							<div class="item-pic" style = " background-image: url('../images/anh1.png'); background-position: 50% 50% !important; " >
+							</div>
+						 </a>
+					</div>
+
+					<div class="col-md-4 item-list-cat">
+						<div class="item-top">
+							<a href="#">Art</a>
+							<button class="detail">
+								<i class="fa fa-plus-circle">
+								</i>
+							</button>
+						</div>
+						<a href="https://medium.com/topic/art" class="">
+							<div class="item-pic" style = " background-image: url('../images/anh1.png'); background-position: 50% 50% !important; " >
+							</div>
+						 </a>
+					</div>
+
+					<div class="col-md-4 item-list-cat">
+						<div class="item-top">
+							<a href="#">Art</a>
+							<button class="detail">
+								<i class="fa fa-plus-circle">
+								</i>
+							</button>
+						</div>
+						<a href="https://medium.com/topic/art" class="">
+							<div class="item-pic" style = " background-image: url('../images/anh1.png'); background-position: 50% 50% !important; " >
+							</div>
+						 </a>
+					</div>
+
+					<div class="col-md-4 item-list-cat">
+						<div class="item-top">
+							<a href="#">Art</a>
+							<button class="detail">
+								<i class="fa fa-plus-circle">
+								</i>
+							</button>
+						</div>
+						<a href="https://medium.com/topic/art" class="">
+							<div class="item-pic" style = " background-image: url('../images/anh1.png'); background-position: 50% 50% !important; " >
+							</div>
+						 </a>
+					</div>
+
+				</div>
+			</section>			
+		
+
+		</div>
+
+		<a href="#" style = "color: #ccc; font-size: 24px;"><i class="sc fa fa-angle-up" id = "cuon" aria-hidden="true"></i></a>
+
+	</div>	
+	<?php include 'footer.php'; ?>
+	<?php include 'script.php'; ?>
+</body>
+
+</html>
